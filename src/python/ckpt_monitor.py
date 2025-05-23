7c74d01c1396d15df71e06ccecfb90b5528fcab5
tungdt <dttvn0010@gmail.com>
2025-05-23 14:57:05 +0700

    Add ckpt_monitor.py

---
+++ src/python/ckpt_monitor.py
@@ -0,0 +1,26 @@
+
+#export CUDA_VISIBLE_DEVICES = 
+
+import os
+import time
+
+ckpt_path = '/root/train/lightning_logs/version_0/checkpoints'
+last_file = ''
+while True:
+   if not os.path.exists(ckpt_path):
+      time.sleep(1)
+      continue
+   
+   files = os.listdir(ckpt_path)
+   if len(files) == 0 or files[0] == last_file:
+      time.sleep(1)
+      continue
+   
+   last_file = files[0]
+   ep = int(last_file.split('=')[1].split('-')[0])
+   if ep % 1 != 0:
+      time.sleep(1)
+      continue
+
+   time.sleep(10)
+   os.system(f'python3 -m piper_train.export_onnx {ckpt_path}/{last_file} /root/onnx/{last_file}.onnx')
